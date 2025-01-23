476a23367dffa04d95192d7d51972d8308cf47e4
fxx <394967886@qq.com>
2025-01-24 11:47:15 +1300

    add BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //无返回值的写入
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+    //有返回值的写入
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //有返回值的查询
+    public interface IQuery<out TResponse>:IRequest<TResponse> where TResponse : notnull
+    {
+
+    }
+}
