d6e181541e415eccae67229ed195da5620458d8b
fxx <394967886@qq.com>
2025-01-24 21:49:34 +1300

    createProduct add

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,21 +1,21 @@
-﻿using BuildingBlock.CQRS;
+﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
 using MediatR;
 
-
-namespace Catalog.Entities.API.Products.CreateProduct;
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-: ICommand<CreateProductResult>;
-
+    : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-
 
 internal class CreateProductCommandHandler
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        //create Product entity from command object
+        //save to database
+        //return CreateProductResult result
 
         var product = new Product
         {
@@ -25,6 +25,10 @@ internal class CreateProductCommandHandler
             ImageFile = command.ImageFile,
             Price = command.Price
         };
+
+        // TODO
+        //save to database
+        //return result
 
         return new CreateProductResult(Guid.NewGuid());
     }
